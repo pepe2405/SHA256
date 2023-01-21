b2b5728e9b20c0b559b176b456aba58b3533ae2b
Preslav Petrov <preslav2405@gmail.com>
2023-01-21 19:25:49 +0200

    Making the code more readablea and cleaner

--- SHA256/Source.cpp
+++ SHA256/Source.cpp
@@ -2,8 +2,9 @@
 
 using namespace std;
 
+//CONSTANTS
 uint32_t H[8] = { 0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19 };
-uint32_t k[64] = { 0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+uint32_t K[64] = { 0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
 						0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
 						0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
 						0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
@@ -11,55 +12,44 @@ uint32_t k[64] = { 0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0
 						0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
 						0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
 						0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2 };
+uint32_t state[8]{ H[0], H[1], H[2], H[3], H[4], H[5], H[6], H[7] };
+const int CHUNK_SIZE = 512;
 
-//Bit functions
-uint32_t sigma1(uint32_t x)
+//BIT FUNCTIONS
+uint32_t rotateRight(const uint32_t x, const int amount)
 {
-	const uint32_t rotateRight17 = (x >> 17) | (x << 15);
-	const uint32_t rotateRight19 = (x >> 19) | (x << 13);
-	const uint32_t shiftRight10 = (x >> 10);
-
-	return rotateRight17 ^ rotateRight19 ^ shiftRight10;
+	return (x >> amount) | (x << (32 - amount));
 }
-
-uint32_t sigma0(uint32_t x)
+uint32_t shiftRight(const uint32_t x, const int amount)
 {
-	const uint32_t rotateRight7 = (x >> 7) | (x << 25);
-	const uint32_t rotateRight18 = (x >> 18) | (x << 14);
-	const uint32_t shiftRight3 = (x >> 3);
-
-	return rotateRight7 ^ rotateRight18 ^ shiftRight3;
+	return (x >> amount);
 }
-
-uint32_t bigSigma1(uint32_t x)
+uint32_t sigma1(const uint32_t x)
 {
-	const uint32_t rotateRight6 = (x >> 6) | (x << 26);
-	const uint32_t rotateRight11 = (x >> 11) | (x << 21);
-	const uint32_t rotateRight25 = (x >> 25) | (x << 7);
-
-	return rotateRight6 ^ rotateRight11 ^ rotateRight25;
+	return rotateRight(x, 17) ^ rotateRight(x, 19) ^ shiftRight(x, 10);
 }
-
-uint32_t bigSigma0(uint32_t x)
+uint32_t sigma0(const uint32_t x)
 {
-	const uint32_t rotateRight2 = (x >> 2) | (x << 30);
-	const uint32_t rotateRight13 = (x >> 13) | (x << 19);
-	const uint32_t rotateRight22 = (x >> 22) | (x << 10);
-
-	return rotateRight2 ^ rotateRight13 ^ rotateRight22;
+	return rotateRight(x, 7) ^ rotateRight(x, 18) ^ shiftRight(x, 3);
 }
-
+uint32_t bigSigma1(const uint32_t x)
+{
+	return rotateRight(x, 6) ^ rotateRight(x, 11) ^ rotateRight(x, 25);
+}
+uint32_t bigSigma0(const uint32_t x)
+{
+	return rotateRight(x, 2) ^ rotateRight(x, 13) ^ rotateRight(x, 22);
+}
 uint32_t major(const uint32_t a, const uint32_t b, const uint32_t c)
 {
 	return (a & b) ^ (a & c) ^ (b & c);
 }
-
-uint32_t change(const uint32_t a, const uint32_t b, const uint32_t c)
+uint32_t choice(const uint32_t a, const uint32_t b, const uint32_t c)
 {
 	return (a & b) ^ ((~a) & c);
 }
 
-//Common methods needed
+//COMMON METHODS
 //get length of a string
 int len(const char* string)
 {
@@ -71,8 +61,7 @@ int len(const char* string)
 	return length;
 }
 
-//concat the second value with the first one from given index
-void concat(int* first, const int startIndex, const int* second, int secondLen)
+void concat(int* first, const int startIndex, const int* second, const int secondLen)
 {
 	for (int i = 0; i < secondLen; i++)
 	{
@@ -80,7 +69,7 @@ void concat(int* first, const int startIndex, const int* second, int secondLen)
 	}
 }
 
-void concat(char* first, const int startIndex, const char* second, int secondLen)
+void concat(char* first, const int startIndex, const char* second, const int secondLen)
 {
 	for (int i = 0; i < secondLen; i++)
 	{
@@ -96,20 +85,12 @@ void copyPartOfString(int* first, const int* second, const int startIndex, const
 	}
 }
 
-void convertLastLineToAppend(int n, int* binary)
-{
-	for (int i = 63; i >= 0; i--)
-	{
-		binary[i] = n % 2;
-		n /= 2;
-	}
-}
-
-//Methods converting Word to Binary
-void convertLetterToBinary(const char letter, int* binary)
+//Methods converting String to Binary
+void convertLetterTo8BitBinary(const char letter, int* binary)
 {
 	int charNum = letter;
-	for (int i = 7; i >= 0; i--)
+	const int LAST_INDEX = 7;
+	for (int i = LAST_INDEX; i >= 0; i--)
 	{
 		binary[i] = charNum % 2;
 		charNum /= 2;
@@ -118,18 +99,25 @@ void convertLetterToBinary(const char letter, int* binary)
 
 void convertWordToBinary(const char* word, int* binaryArray)
 {
-	const int wordLength = len(word);
-	const int binaryLength = 8 * wordLength;
+	const int WORD_LENGTH = len(word);
 	
-	for (int i = 0; i < wordLength; i++)
+	for (int i = 0; i < WORD_LENGTH; i++)
 	{
-		int* letterBinary = new int[8]{};
-		convertLetterToBinary(word[i], letterBinary);
+		int letterBinary[8]{};
+		convertLetterTo8BitBinary(word[i], letterBinary);
 
-		const int currentIndex = i * 8;
-		concat(binaryArray, currentIndex, letterBinary, 8);
-		
-		delete[] letterBinary;
+		const int CURRENT_INDEX = i * 8;
+		concat(binaryArray, CURRENT_INDEX, letterBinary, 8);
+	}
+}
+
+void convertLastLineToAppend(int n, int* binary)
+{
+	const int LAST_INDEX = 63;
+	for (int i = LAST_INDEX; i >= 0; i--)
+	{
+		binary[i] = n % 2;
+		n /= 2;
 	}
 }
 
@@ -138,11 +126,11 @@ void convertStringTo32BitChunks(const int* string, uint32_t* chunkWords)
 {
 	for (int i = 0; i < 16; i++)
 	{
-		int start = i * 32;
-		int end = i * 32 + 32 - 1;
+		const int START = i * 32;
+		const int END = i * 32 + 32 - 1;
 		int n = 0;
 		int adder = 1;
-		for (int j = end; j >= start; j--)
+		for (int j = END; j >= START; j--)
 		{
 			n += (string[j] * adder);
 			adder *= 2;
@@ -162,50 +150,82 @@ void createMessageSchedule(uint32_t* w)
 	}
 }
 
-void compressAndModify(const uint32_t* w)
+void compress(const uint32_t* w)
 {
-	uint32_t a = H[0];
-	uint32_t b = H[1];
-	uint32_t c = H[2];
-	uint32_t d = H[3];
-	uint32_t e = H[4];
-	uint32_t f = H[5];
-	uint32_t g = H[6];
-	uint32_t h = H[7];
 	//Compression loop
-	for (int i = 0; i < 64; i++)
+	const int LAST_INDEX = 64;
+	for (int i = 0; i < LAST_INDEX; i++)
 	{
-		const uint32_t S1 = bigSigma1(e);
-		const uint32_t ch = change(e, f, g);
-		const uint32_t temp1 = h + S1 + ch + k[i] + w[i];
-		const uint32_t S0 = bigSigma0(a);
-		const uint32_t maj = major(a, b, c);
+		const uint32_t S1 = bigSigma1(H[4]);
+		const uint32_t ch = choice(H[4], H[5], H[6]);
+		const uint32_t temp1 = H[7] + S1 + ch + K[i] + w[i];
+		const uint32_t S0 = bigSigma0(H[0]);
+		const uint32_t maj = major(H[0], H[1], H[2]);
 		const uint32_t temp2 = S0 + maj;
-		h = g;
-		g = f;
-		f = e;
-		e = d + temp1;
-		d = c;
-		c = b;
-		b = a;
-		a = temp1 + temp2;
+		H[7] = H[6];
+		H[6] = H[5];
+		H[5] = H[4];
+		H[4] = H[3] + temp1;
+		H[3] = H[2];
+		H[2] = H[1];
+		H[1] = H[0];
+		H[0] = temp1 + temp2;
 	}
-
-	//Modifying final values
-	H[0] = H[0] + a;
-	H[1] = H[1] + b;
-	H[2] = H[2] + c;
-	H[3] = H[3] + d;
-	H[4] = H[4] + e;
-	H[5] = H[5] + f;
-	H[6] = H[6] + g;
-	H[7] = H[7] + h;
 }
 
-//Convert the int into hex and print
+void modify()
+{
+	//Modifying final values
+	H[0] = H[0] + state[0];
+	H[1] = H[1] + state[1];
+	H[2] = H[2] + state[2];
+	H[3] = H[3] + state[3];
+	H[4] = H[4] + state[4];
+	H[5] = H[5] + state[5];
+	H[6] = H[6] + state[6];
+	H[7] = H[7] + state[7];
+}
+
+//Initializing binary arrays
+void initializeFirstBinary(int* firstBinary, const char* input)
+{
+	convertWordToBinary(input, firstBinary);
+}
+
+void initializeSecondBinary(int* secondBinary, const int* firstBinary, const int binaryLength, const int newLength)
+{
+	concat(secondBinary, 0, firstBinary, binaryLength);
+	secondBinary[binaryLength] = 1;
+	int binaryToAppend[64]{};
+	convertLastLineToAppend(binaryLength, binaryToAppend);
+	concat(secondBinary, newLength - 64, binaryToAppend, 64);
+}
+
+//Chunk Method
+void chunkLoop(const int chunkCount, const int* secondBinary)
+{
+	for (int i = 0; i < chunkCount; i++)
+	{
+		//Creating the 64 array of 32 bits
+		uint32_t w[64]{};
+		int messageToConvert[CHUNK_SIZE]{};
+		const int currentStartIndex = (i + 1) * CHUNK_SIZE;
+		copyPartOfString(messageToConvert, secondBinary, i * CHUNK_SIZE, CHUNK_SIZE);
+		convertStringTo32BitChunks(messageToConvert, w);
+
+		//Message schedule
+		createMessageSchedule(w);
+
+		//Compress and modify
+		compress(w);
+		modify();
+	}
+}
+
+//Hex methods
 void decToHex(uint32_t n, char* hex)
 {
-	int i = 7;
+	int index = 7;
 	while (n != 0) {
 		
 		int rem = 0;
@@ -217,13 +237,12 @@ void decToHex(uint32_t n, char* hex)
 		else {
 			ch = rem + 87;
 		}
-		hex[i] = ch;
+		hex[index] = ch;
 		n = n / 16;
-		i--;
+		index--;
 	}
 }
 
-//Print the hex
 void printHex(const char* hex)
 {
 	const int length = len(hex);
@@ -233,63 +252,47 @@ void printHex(const char* hex)
 	}
 }
 
+void createFinalHex(char* finalHex)
+{
+	
+	char hexString[9]{};
+	for (int i = 0; i < 8; i++)
+	{
+		decToHex(H[i], hexString);
+		concat(finalHex, i * 8, hexString, 8);
+	}
+}
+
 
 
 int main()
 {
-	//Creating the first binary block (only the string in binary)
-	char word[10000];
-	cin.getline(word, 10000);
-	const int wordLength = len(word);
-	const int binaryLength = wordLength * 8;
-	int* firstBinary = new int[binaryLength + 1]{};
-	convertWordToBinary(word, firstBinary);
-	
-	//Creating the second binary block
-	int k = 0;
+	//STEP 1 - PRE PROCESSING
+	char input[10000];
+	cin.getline(input, 10000);
 
-	while((binaryLength + k + 1 + 64) % 512 != 0)
-	{
-		k++;
-	}
-	const int newLength = binaryLength + k + 65;
+	//STEP 2 - Creating first binary block
+	const int STRING_LENGTH = len(input);
+	const int BINARY_LENGTH = STRING_LENGTH * 8;
+	int* firstBinary = new int[BINARY_LENGTH + 1]{};
+	initializeFirstBinary(firstBinary, input);
 
-	//Appending the zeros and the last line
-	int* secondBinaryBlock = new int[newLength] {};
-	concat(secondBinaryBlock, 0, firstBinary, binaryLength);
-	secondBinaryBlock[binaryLength] = 1;
-	
-	int binaryToAppend[64]{};
-	convertLastLineToAppend(binaryLength, binaryToAppend);
-	concat(secondBinaryBlock, newLength - 64, binaryToAppend, 64);
-	
-	//Now we have the %512 length block
-	const int chunkNumbers = newLength / 512;
+	//STEP 3 - Creating the second binary block
+	const int remainder = (BINARY_LENGTH + 65) / CHUNK_SIZE;
+	const int NEW_LENGTH = (remainder + 1) * CHUNK_SIZE;
+	int* secondBinaryBlock = new int[NEW_LENGTH] {};
+	initializeSecondBinary(secondBinaryBlock, firstBinary, BINARY_LENGTH, NEW_LENGTH);
 
-	//For each chunk
-	for (int i = 0; i < chunkNumbers; i++)
-	{
-		//Creating the 64 array of 32 bits
-		uint32_t w[64]{};
-		int messageToConvert[512]{};
-		const int currentStartIndex = (i + 1) * 512;
-		copyPartOfString(messageToConvert, secondBinaryBlock, i * 512, 512);
-		convertStringTo32BitChunks(messageToConvert, w);
+	//STEP 4 - CHUNK LOOP
+		//STEP 5 - Creating message schedule
+		//STEP 6 - Modifying and compressing
+	const int CHUNK_COUNT = NEW_LENGTH / CHUNK_SIZE;
+	chunkLoop(CHUNK_COUNT, secondBinaryBlock);
 
-		//Message schedule
-		createMessageSchedule(w);
-
-		//Compression
-		compressAndModify(w);
-	}
-
-	int index = 0;
-	char hexString[20]{};
-	for (int i = 0; i < 8; i++)
-	{
-		decToHex(H[i], hexString);
-		printHex(hexString);
-	}
+	//STEP 7 - Concatenate final hash and print it
+	char finalHexString[65]{};
+	createFinalHex(finalHexString);
+	printHex(finalHexString);
 
 	delete[] firstBinary;
 	delete[] secondBinaryBlock;
